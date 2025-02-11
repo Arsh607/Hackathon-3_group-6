159bebf13aec90d731642036d411bc861a91ca72
unknown <rohledera@hotmail.com>
2025-02-11 17:10:22 -0600

    BRANCH: javascript-ashleyr added helper function for the interactive answer buttons

---
+++ javascript.js
@@ -0,0 +1,56 @@
+//quiz array
+const quiz = [
+    { 
+        question: "Which river is NOT one of the two main rivers that meet in Winnipeg?", 
+        options: ["Red River", "Assiniboine River", "Saskatchewan River"],
+        correct: 1  // Index of correct answer
+    },
+    { 
+        question: "What is one of Winnipeg's nicknames?", 
+        options: ["The Peg", "Prairie City", "The Gateway"], 
+        correct: 0  // Index of correct answer
+    },
+    { 
+        question: "Which NHL team is based in Winnipeg?", 
+        options: ["Calgary Flames", "Edmonton Oilers", "The Jets"], 
+        correct: 2  // Index of correct answer
+    },
+    { 
+        question: "Which famous Canadian museum is located in Winnipeg?", 
+        options: ["Royal Museum", "National Gallary of Canada", "Canadian Museum for Human Rights"], 
+        correct: 2 // Index of correct answer
+    },
+    { 
+        question: "What is the coldest temperature ever recorded in Winnipeg?", 
+        options: ["-25", "-45", "-34"], 
+        correct: 1  // Index of correct answer
+    },
+    { 
+        question: "What is the name of Winnipeg's international airport?", 
+        options: ["John Diefenbaker International Airport", "Lester B. Pearson International Airport", "James Armstrong Richardson International Airport"], 
+        correct: 2  // Index of correct answer
+    },
+    { 
+        question: "Which famous bear was named after Winnipeg?", 
+        options: ["Paddington Bear", "Winnie the Pooh", "Yogi Bear"], 
+        correct: 1  // Index of correct answer
+    },
+]
+
+// sets the current state of the quiz
+let currentState = 0
+
+// helper function that creates the buttons for each answer option. 
+function addAnswerButton(text, nextState) {
+    //creates the new button element
+    const button = document.createElement("button");
+
+     // Adds an 'option-button' class for styling purposes
+    button.classList.add("option-button");
+
+    // Adds the button to the options container
+    const answersBox = document.getElementById("answers");
+    answersBox.appendChild(button);
+
+
+//function that iterates through the quiz array and confirms correct answers. Provides feedback for wrong answers. 
